b5a3f3581665b0b78e7f2f578dafc93324e430e6
Niels Voogt <niels@fitmanager.com>
2019-08-15 12:06:07 +0200

    Migrated to gulp - webpack wasnt really neccesary for this

---
+++ gulpfile.js
@@ -0,0 +1,62 @@
+'use strict';
+
+var gulp = require('gulp'),
+    notify = require('gulp-notify'),
+    watch = require('gulp-watch'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    merge = require('merge-stream');
+
+function onError(err) {
+    console.log(err);
+    this.emit('end');
+}
+
+// --------------------------------------------------- SASS/CSS DEST
+
+var sassInput = './scss/**/*.scss',
+    // cssInput = './public/css/*.css',
+    devInput = './public/*.html';
+
+// --------------------------------------------------- COMPILE OPTIONS
+
+var sassOptions = {
+    errLogToConsole: true,
+    outputStyle: 'expanded'
+};
+
+// --------------------------------------------------- AUTOPREFIXER OPTIONS
+
+var autoprefixerOptions = {
+    browsers: ['last 2 versions', '> 1%', 'Safari 8', 'Firefox ESR']
+};
+
+// --------------------------------------------------- SASS TASK
+
+gulp.task('sass', () => {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest('./public/css/'));
+});
+
+// --------------------------------------------------- BROWSERSYNC TASK (combines 'sass' with 'browsersync')
+
+gulp.task('serve', gulp.series('sass', () => {
+
+    browserSync.init({
+        browser: ["chrome"],
+        server: {
+            baseDir: 'public/'
+        },
+    });
+
+    gulp.watch(sassInput, gulp.series('sass'));
+    gulp.watch(sassInput).on('change', browserSync.reload);
+    gulp.watch(devInput).on('change', browserSync.reload);
+}));
+
+gulp.task('watch', gulp.series('sass', () => {
+    gulp.watch(sassInput, gulp.series('sass'));
+}));
--- src/scripts/index.js
+++
@@ -1,5 +0,0 @@
-import '../styles/index.scss';
-import Prism from 'prismjs';
-
-// Highlight all matching syntax
-Prism.highlightAll();
